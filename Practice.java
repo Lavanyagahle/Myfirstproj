ad9c721eb33547fbe21b39d1fccc93ba10cbfc66
Lavanyagahle <135302116+Lavanyagahle@users.noreply.github.com>
2023-06-06 18:05:22 +0530

    Add files via upload

---
+++ Practice.java
@@ -0,0 +1,11 @@
+package com.firstproj;
+
+public class Practice {
+public static void main(String[] args) {
+	System.out.println("Hello "+2+3);
+	System.out.println(2+3+" hello");
+	System.out.println((2+3)+" hello");
+	System.out.println("Hello "+(2+3));
+	System.out.println(2+3+"Hello "+2+3);
+}
+}
